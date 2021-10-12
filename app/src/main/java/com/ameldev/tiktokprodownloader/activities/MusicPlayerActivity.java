57c17182235ea5ed77d1bab5251f21b468d3e603
benidarma <91493222+benidarma@users.noreply.github.com>
2021-10-12 16:47:59 +0700

    Update MusicPlayerActivity.java

--- app/src/main/java/com/ameldev/tiktokprodownloader/activities/MusicPlayerActivity.java
+++ app/src/main/java/com/ameldev/tiktokprodownloader/activities/MusicPlayerActivity.java
@@ -3,10 +3,12 @@ package com.ameldev.tiktokprodownloader.activities;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.SeekBar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.databinding.DataBindingUtil;
@@ -14,9 +16,12 @@ import androidx.databinding.DataBindingUtil;
 import com.ameldev.tiktokprodownloader.R;
 import com.ameldev.tiktokprodownloader.databinding.ActivityMusicPlayerBinding;
 import com.github.abdularis.buttonprogress.DownloadButtonProgress;
-import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdError;
 import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.FullScreenContentCallback;
+import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
 import com.jaeger.library.StatusBarUtil;
 
 import java.io.IOException;
@@ -127,12 +132,23 @@ public class MusicPlayerActivity extends AppCompatActivity implements MediaPlaye
     }
 
     private void buildInterstitialAds() {
-        mInterstitial = new InterstitialAd(this);
-        mInterstitial.setAdUnitId(getString(R.string.admob_interstitial_id));
+        AdRequest adRequest = new AdRequest.Builder().build();
 
-        mInterstitial.loadAd(new AdRequest.Builder()
-                //.addTestDevice("") //ca-app-pub-3940256099942544/1033173712
-                .build());
+        InterstitialAd.load(this, getString(R.string.admob_interstitial_id), adRequest,
+                new InterstitialAdLoadCallback() {
+                    @Override
+                    public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
+                        mInterstitial = interstitialAd;
+                        Log.i("TAG", "onAdLoaded");
+                    }
+
+                    @Override
+                    public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                        // Handle the error
+                        Log.i("TAG", loadAdError.getMessage());
+                        mInterstitial = null;
+                    }
+                });
     }
 
     private int musiccurrentposition() {
@@ -193,15 +209,24 @@ public class MusicPlayerActivity extends AppCompatActivity implements MediaPlaye
 
     @Override
     public void onBackPressed() {
-        if (mInterstitial.isLoaded()) {
-            mInterstitial.show();
-            mInterstitial.setAdListener(new AdListener() {
+        if (mInterstitial != null) {
+            mInterstitial.setFullScreenContentCallback(new FullScreenContentCallback() {
                 @Override
-                public void onAdClosed() {
-                    super.onAdClosed();
+                public void onAdFailedToShowFullScreenContent(@NonNull AdError adError) {
+                    finish();
+                }
+
+                @Override
+                public void onAdShowedFullScreenContent() {
+
+                }
+
+                @Override
+                public void onAdDismissedFullScreenContent() {
                     finish();
                 }
             });
+            mInterstitial.show(MusicPlayerActivity.this);
         } else {
             super.onBackPressed();
         }
