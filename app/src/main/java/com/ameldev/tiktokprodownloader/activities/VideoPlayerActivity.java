dbee7cc1f17170c44dc20536e2dfe516c2e975f4
benidarma <91493222+benidarma@users.noreply.github.com>
2021-10-12 16:48:19 +0700

    Update VideoPlayerActivity.java

--- app/src/main/java/com/ameldev/tiktokprodownloader/activities/VideoPlayerActivity.java
+++ app/src/main/java/com/ameldev/tiktokprodownloader/activities/VideoPlayerActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
@@ -11,15 +12,19 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.MediaController;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.databinding.DataBindingUtil;
 
 import com.ameldev.tiktokprodownloader.R;
 import com.ameldev.tiktokprodownloader.databinding.ActivityVideoPlayerBinding;
-import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdError;
 import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.FullScreenContentCallback;
+import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
 import com.jaeger.library.StatusBarUtil;
 
 import java.util.Objects;
@@ -118,12 +123,23 @@ public class VideoPlayerActivity extends AppCompatActivity {
     }
 
     private void buildInterstitialAds() {
-        mInterstitial = new InterstitialAd(this);
-        mInterstitial.setAdUnitId(getString(R.string.admob_interstitial_id));
+        AdRequest adRequest = new AdRequest.Builder().build();
 
-        mInterstitial.loadAd(new AdRequest.Builder()
-                //.addTestDevice("") //ca-app-pub-3940256099942544/1033173712
-                .build());
+        InterstitialAd.load(this, getString(R.string.admob_interstitial_id), adRequest,
+                new InterstitialAdLoadCallback() {
+                    @Override
+                    public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
+                        mInterstitial = interstitialAd;
+                        Log.i("TAG", "onAdLoaded");
+                    }
+
+                    @Override
+                    public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                        // Handle the error
+                        Log.i("TAG", loadAdError.getMessage());
+                        mInterstitial = null;
+                    }
+                });
     }
 
     private void HideOrShow() {
@@ -146,15 +162,24 @@ public class VideoPlayerActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        if (mInterstitial.isLoaded()) {
-            mInterstitial.show();
-            mInterstitial.setAdListener(new AdListener() {
+        if (mInterstitial != null) {
+            mInterstitial.setFullScreenContentCallback(new FullScreenContentCallback() {
                 @Override
-                public void onAdClosed() {
-                    super.onAdClosed();
+                public void onAdFailedToShowFullScreenContent(@NonNull AdError adError) {
+                    finish();
+                }
+
+                @Override
+                public void onAdShowedFullScreenContent() {
+
+                }
+
+                @Override
+                public void onAdDismissedFullScreenContent() {
                     finish();
                 }
             });
+            mInterstitial.show(VideoPlayerActivity.this);
         } else {
             super.onBackPressed();
         }
